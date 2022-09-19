afb7eeff4953a20673bb62e76b0a4ce91d2771cc
Augenstern9 <65142123+Augenstern9@users.noreply.github.com>
2022-09-19 16:21:39 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -82,14 +82,14 @@ export const config = {
       //  {"type": "节日", "name": "3", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "老婆", "year": "1996", "date": "06-11"},
-       // {"type": "认识", "name": "我们", "year": "2021", "date": "10-18"},
+        {"type": "认识", "name": "我们", "year": "2021", "date": "10-18"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
         {"keyword": "love_day", date: "2021-12-12"},
         // 结婚纪念日
-       // {"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: "2022-09-09"},
         // 退伍日
        // {"keyword": "ex_day", date: "2022-09-10"},
       ]

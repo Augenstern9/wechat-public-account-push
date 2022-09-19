328a78d20840b94df271872f3bb50f73c66c1947
Augenstern9 <65142123+Augenstern9@users.noreply.github.com>
2022-09-19 16:25:45 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -70,7 +70,7 @@ export const config = {
       // 所在城市
       city: "杭州",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '06-11',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -89,7 +89,7 @@ export const config = {
         // 在一起的日子
         {"keyword": "love_day", date: "2021-12-12"},
         // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: "2021-10-18"},
         // 退伍日
        // {"keyword": "ex_day", date: "2022-09-10"},
       ]
